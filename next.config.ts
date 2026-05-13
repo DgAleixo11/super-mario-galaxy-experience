af9b6f9114d1f47d421b8a44b6840d9cb3a3f046
Diogo Aleixo de Andrade <133553343+DgAleixo11@users.noreply.github.com>
2026-05-13 18:40:59 +0000

    Configura deploy no GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/super-mario-galaxy-experience",
+  assetPrefix: "/super-mario-galaxy-experience/",
 };
 
 export default nextConfig;
